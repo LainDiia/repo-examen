a4b798f33fef95c9ee7a2b6d1879fef9b878e478
LainDiia <colombia0610@gmail.com>
2022-10-27 19:56:57 +0200

    #git all
    cambios

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿//1.Haz un programa en c# que calcule la solución de la ecuación de 
+//  primer grado. ax+b=0. Hay que controlar cuando la a es cero.
+
+//Declarar variables.
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A, B, X;
+
+        //Pedir datos
+        Console.WriteLine("Escribe el valor para A");
+        A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Escribe el valor para B");
+        B = Convert.ToInt32(Console.ReadLine());
+
+        //Cálculo
+
+        if (A == 0)
+        {
+            Console.WriteLine("ERROR");
+        }
+        else
+        {
+            X = (X = -B / A);
+
+            Console.WriteLine($"El resultado de la ecuacion es   + {X}");
+        }
+    }
+}
---
+++ Prova Sistemes Operatius/Prova Sistemes Operatius/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prova_Sistemes_Operatius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
